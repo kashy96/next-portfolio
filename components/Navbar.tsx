69fa5375e737e778ac1124a26301ca10720925c6
Kashif Khan <kashif@thegemcloud.com>
2025-01-15 11:50:11 +0500

    ts config issues

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import { Menu, MenuItem } from "../components/ui/navbar-menu";
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
 export function Navbar({
   scrollToSection,
--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  "compilerOptions": {
-    "baseUrl": "./",
-    "paths": {
-      "@/*": ["./app/*"]
-    }
-  }
 };
 
 export default nextConfig;
