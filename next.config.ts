6bf7c4664cd293590a1b6dae0875d17ef3a7a589
Kashif Khan <kashif@thegemcloud.com>
2025-01-15 11:38:19 +0500

    issue resolved

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  "compilerOptions": {
+    "baseUrl": "./",
+    "paths": {
+      "@/*": ["./app/*"]
+    }
+  }
 };
 
 export default nextConfig;
